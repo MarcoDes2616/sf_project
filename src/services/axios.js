5e6cc60074feeda0ccea16f9cd2247d81fa69bee
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-12-01 12:01:41 -0400

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import authService from "./authServices";
 
-const BASE_URL = ""
 const BASE_URL_LOCAL = import.meta.env.VITE_LOCAL_API_URL
+const BASE_URL_PROD = import.meta.env.VITE_PROD_API_URL
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL_LOCAL,
+    baseURL: BASE_URL_PROD,
 })
     axiosInstance.interceptors.request.use((config) => {
         config.headers = {Authorization : "Bearer " + authService.getCurrentUser()}
