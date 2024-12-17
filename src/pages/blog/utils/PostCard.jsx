984f04636ac9f36c4fc51470270922ceeffb3cd7
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-12-17 15:54:54 -0400

    update premium section

--- src/pages/blog/utils/PostCard.jsx
+++ src/pages/blog/utils/PostCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import DiamondIcon from '@mui/icons-material/Diamond';
 
-const PostCard = (post) => {
+const PostCard = (post, premium) => {
+    
     const {id, title, description, imageUrl, tag, user, createdAt} = post
     const date = new Date(createdAt).toLocaleString();
     const summary = description.slice(0, 50) + "..."
@@ -8,7 +10,7 @@ const PostCard = (post) => {
         <div className="post_card">
             <img src={imageUrl} alt={title} className="post_card_image" />
             <div className="post_card_content">
-                <h3 className="post_card_title">{title}</h3>
+                <h3 className="post_card_title">{title} {premium && <DiamondIcon />}</h3>
                 <p className="post_card_category">{tag.tag}</p>
                 <p className="post_card_author">Por {user.name + " " + user.lastname}</p>
                 <p className="post_card_date">{date}</p>
--- src/pages/virtual_school/tabs/my_courses/MyCourses.jsx
+++ src/pages/virtual_school/tabs/my_courses/MyCourses.jsx
@@ -4,7 +4,6 @@ import CoursesContain from "../../utils/CoursesContain";
 import Loading from "/src/components/Loading";
 import PostCard from "../../../blog/utils/PostCard";
 import DiamondIcon from '@mui/icons-material/Diamond';
-import PrimaryBtn from "../../../../components/generals/PrimaryBtn";
 import YellowBtn from "../../../../components/generals/YellowBtn";
 
 function MyCourses() {
@@ -23,11 +22,11 @@ function MyCourses() {
       <CoursesContain {...propsToCourses} />
       <br /><br />
       <div className="full-w autoM x-big">
-        <h3 style={{color: "#0F3F76"}}>Señales de mercado<DiamondIcon /></h3>
+        <h3 style={{color: "#0F3F76"}}>Señales del mercado de Sintéticos</h3>
         {allContain[0] ? (
           <div className="blog_premium post_cards_container">
             {allPosts.map((post, i) => (
-              <PostCard key={i} {...post} />
+              <PostCard key={i} {...post} premium={true} />
             ))}
           </div>
         ) : (
